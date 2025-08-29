a06400eac5271dc9329233d1b8ad9710d9f60cbf
unknown <joaomasgosandoval@gmail.com>
2025-08-29 17:58:54 -0300

    Pelicula inicializandose

---
+++ src/main/java/ar/edu/unahur/obj2/wtoj/contenido/Pelicula.java
@@ -0,0 +1,5 @@
+package ar.edu.unahur.obj2.wtoj.contenido;
+
+public class Pelicula {
+
+}
---
+++ src/test/java/ar/edu/unahur/obj2/wtoj/contenido/PeliculaTest.java
@@ -0,0 +1,14 @@
+package ar.edu.unahur.obj2.wtoj.contenido;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class PeliculaTest {
+    @Test
+    public void testQueCalculaCostoLicencia() {
+        //Preparacion (Armo el escenario)
+        //Ejecucion (Invoco el comportamiento a probar)
+        //Contractacion (Verifico que lo que espero que suceda vaya a suceder)
+    }
+}
