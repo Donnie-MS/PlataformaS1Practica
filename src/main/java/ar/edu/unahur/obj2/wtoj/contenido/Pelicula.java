43bc1a4d0bfc29304fc5782f3618bfbae61c21e7
Orejas 2016 <joaomasgosandoval@gmail.com>
2025-09-01 22:39:29 -0300

    Test de plataforma terminado

--- src/main/java/ar/edu/unahur/obj2/wtoj/contenido/Pelicula.java
+++ src/main/java/ar/edu/unahur/obj2/wtoj/contenido/Pelicula.java
@@ -1,5 +1,14 @@
 package ar.edu.unahur.obj2.wtoj.contenido;
 
 public class Pelicula {
+    private String titulo;
+    private Double costoBase;
 
+    public Pelicula(String titulo, double costoBase) {
+        this.titulo = titulo;
+        this.costoBase = costoBase;
+    }
+    public Double calcularCostoLicencia() {
+        return costoBase;
+    }
 }
--- src/test/java/ar/edu/unahur/obj2/wtoj/contenido/PeliculaTest.java
+++ src/test/java/ar/edu/unahur/obj2/wtoj/contenido/PeliculaTest.java
@@ -1,6 +1,6 @@
 package ar.edu.unahur.obj2.wtoj.contenido;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
@@ -8,7 +8,11 @@ public class PeliculaTest {
     @Test
     public void testQueCalculaCostoLicencia() {
         //Preparacion (Armo el escenario)
+        Pelicula pelicula = new Pelicula("Matriz", 3.0);
+        Double valorEsperado = 3.0;
         //Ejecucion (Invoco el comportamiento a probar)
-        //Contractacion (Verifico que lo que espero que suceda vaya a suceder)
+        Double valorObtenido = pelicula.calcularCostoLicencia();
+        //Contrastacion (Verifico que lo que espero que suceda vaya a suceder)
+        assertEquals(valorEsperado, valorObtenido);
     }
 }
